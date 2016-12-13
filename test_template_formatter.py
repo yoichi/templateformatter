0cfd85c7757db533b0eed55c29541409b0cb796f
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2016-12-13 20:58:45 +0900

    reduce code duplication

--- test_template_formatter.py
+++ test_template_formatter.py
@@ -5,15 +5,19 @@ from template_formatter import TemplateFormatter
 
 
 class TestTemplateFormatter(unittest.TestCase):
+    def _test_same_content(self, path1, path2):
+        with open(path1) as f1:
+            c1 = f1.read()
+        with open(path2) as f2:
+            c2 = f2.read()
+        self.assertEqual(c1, c2)
+
     def test_substitute_to_file(self):
         formatter = TemplateFormatter(['t/key.yml'])
         formatter.substitute_to_file('t/ascii.py.template',
                                      't/ascii_output.py')
-        with open('t/ascii.py') as f:
-            expected = f.read()
-        with open('t/ascii_output.py') as f:
-            result = f.read()
-        self.assertEqual(result, expected)
+        self._test_same_content('t/ascii.py',
+                                't/ascii_output.py')
 
     def test_substitute_keyerror(self):
         formatter = TemplateFormatter([])
@@ -24,21 +28,15 @@ class TestTemplateFormatter(unittest.TestCase):
         formatter = TemplateFormatter(['t/key.yml'])
         formatter.substitute_to_file('t/utf8.py.template',
                                      't/utf8_output.py')
-        with open('t/utf8.py') as f:
-            expected = f.read()
-        with open('t/utf8_output.py') as f:
-            result = f.read()
-        self.assertEqual(result, expected)
+        self._test_same_content('t/utf8.py',
+                                't/utf8_output.py')
 
     def test_cp932_to_file(self):
         formatter = TemplateFormatter(['t/key.yml'])
         formatter.substitute_to_file('t/cp932.py.template',
                                      't/cp932_output.py')
-        with open('t/cp932.py') as f:
-            expected = f.read()
-        with open('t/cp932_output.py') as f:
-            result = f.read()
-        self.assertEqual(result, expected)
+        self._test_same_content('t/cp932.py',
+                                't/cp932_output.py')
 
 
 if __name__ == '__main__':
