63d62be36cb13a46c0e0b36b0fb85dc12d28c798
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2020-06-24 18:58:32 +0900

    Run test by GitHub Actions

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='templateformatter',
+    url='https://github.com/yoichi/templateformatter',
+    author='Yoichi Nakayama',
+    author_email='yoichi.nakayama@gmail.com',
+    py_modules=['template_formatter'],
+    test_suite='test_template_formatter'
+)
