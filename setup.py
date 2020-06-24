629a1c73cf767e9b2c4e4cf1479d60659d1c992f
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2020-06-24 19:04:56 +0900

    add pyyaml to tests_require

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setuptools.setup(
     author='Yoichi Nakayama',
     author_email='yoichi.nakayama@gmail.com',
     py_modules=['template_formatter'],
-    test_suite='test_template_formatter'
+    test_suite='test_template_formatter',
+    tests_require=['pyyaml']
 )
